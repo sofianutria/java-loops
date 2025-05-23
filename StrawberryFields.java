b33d477c673d1de641c27779cdf16e79871c7b9d
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-05-23 14:10:58 +0200

    add exercise 4

--- StrawberryFields.java
+++ StrawberryFields.java
@@ -3,30 +3,62 @@ public class StrawberryFields {
     //Escribe un programa que permita imprimir la canción Strawberry Fields Forever con un bucle. Solo puedes tener 2 funciones, una que imprima los versos y otra que imprima el coro.
     //Busca información sobre bucles anidados.
     //Al final está la letra completa
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            playChorus();
+            playVersus(i);
+        }
     }
 
     /**
      * Function name: playVersus
-     * 
+     *
      * @param number (int)
-     * 
+     *
      * Inside the function:
      * 1. print versus according the position number
      */
-    
+
 
     /**
      * Function name: playChorus
-     * 
+     * <p>
      * Inside the function:
-     * 1. print chorus 
+     * 1. print chorus
      */
-    
-
+    public static void playVersus(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("Living is easy with eyes closed\n" +
+                        "Misunderstanding all you see\n" +
+                        "It's getting hard to be someone, but it all works out\n" +
+                        "It doesn't matter much to me");
+                break;
+            case 2:
+                System.out.println("No one I think is in my tree\n" +
+                        "I mean, it must be high or low\n" +
+                        "That is, you can't, you know, tune in, but it's alright\n" +
+                        "That is, I think it's not too bad");
+                break;
+            case 3:
+                System.out.println("Always, no sometimes, think it's me\n" +
+                        "But you know, I know when it's a dream\n" +
+                        "I think I know, I mean a yes\n" +
+                        "But it's all wrong\n" +
+                        "That is, I think I disagree");
+                break;
+        }
+    }
+    public static void playChorus() {
+        System.out.println("Let me take you down\n" +
+                "'Cause I'm going to strawberry fields\n" +
+                "Nothing is real\n" +
+                "And nothing to get hung about\n" +
+                "Strawberry fields forever");
+    }
 }
+
 
 
 /* 
